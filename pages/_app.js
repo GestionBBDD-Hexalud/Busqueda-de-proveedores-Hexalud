42721430d2471ff131e4e722827d2dbb3e4538db
GestionBBDD-Hexalud <gestionbbdd@hexalud.com>
2025-10-28 14:45:07 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
-// /pages/_app.js
+// pages/_app.js
+import Script from "next/script";
 import "mapbox-gl/dist/mapbox-gl.css";
+import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* 🔑 Carga del script de Google Maps con la librería Places */}
+      <Script
+        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
+        strategy="beforeInteractive"
+      />
+
+      {/* 🌐 Componente principal */}
+      <Component {...pageProps} />
+    </>
+  );
 }
